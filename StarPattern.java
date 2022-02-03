a50b0d7e97c2898c8146b072692bb42d19496a4b
AjithAndrewJ <98014510+AjithAndrewJ@users.noreply.github.com>
2022-02-03 11:18:59 +0530

    Add files via upload

--- StarPattern.java
+++ StarPattern.java
@@ -1,26 +1,97 @@
 package basicprogram;
 
+import java.util.Scanner;
+
 public class StarPattern {
 
 	public static void main(String[] args) {
-		char i,j,n,m;
-		for(i=1;i<=8;i++) {
-			for(j=1;j<=8;j++) {
-				System.out.print("*");
+		
+		int row;
+		char ch;
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the number of rows : ");
+		row=in.nextInt();
+		System.out.println("Enter the character : ");
+		ch=in.next().charAt(0);
+		in.close();
+		
+		System.out.println();
+		
+		for(int i=1;i<=row;i++) {
+			for(int j=1;j<=row;j++) {
+				System.out.print(ch);
 			}
-			System.out.println(" ");
+			System.out.println();
 		}
 		
+		System.out.println();
 		
-		System.out.println(" ");
-		
-		
-		for(n=1;n<=5;n++) {
-			for(m=1;m<=n;m++) {
-				System.out.print("*");
+		for(int i=1;i<=row;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(ch);
 			}
-			System.out.println(" ");
+			System.out.println();
 		}
+		
+		System.out.println();
+		
+		for(int i=1;i<=row;i++) {
+			for(int j=row;j>i;j--) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print(ch);
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		for(int i=1;i<=row;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(ch);
+			}
+			System.out.println();
+		}
+		 for(int i=row-1; i>=1; i--) { 
+	         for(int j=1; j<=i; j++){
+	             System.out.print(ch);
+	         }
+	         System.out.println();
+         }
+		 
+		 System.out.println();
+		 
+		 for(int i=1; i<=row; i++) {
+			 for(int j=row; j>i; j--){
+	             System.out.print(" ");
+	         }
+	         for(int k=1; k<=(i*2)-1; k++){
+	             System.out.print(ch);
+	         }
+	         System.out.println();
+	     }
+		 
+		 System.out.println();
+		 
+		 for(int i=1; i<=row; i++) {
+			 for(int j=row; j>i; j--){
+	             System.out.print(" ");
+	         }
+	         for(int k=1; k<=(i*2)-1; k++){
+	             System.out.print(ch);
+	         }
+	         System.out.println();
+	     }
+		 for(int i=row-1; i>=1; i--) {
+			 for(int j=row-1; j>=i; j--){
+	             System.out.print(" ");
+	         }
+	         for(int k=1; k<=(i*2)-1; k++){
+	             System.out.print(ch);
+	         }
+	         System.out.println();
+	     }
 	}
 
 }
